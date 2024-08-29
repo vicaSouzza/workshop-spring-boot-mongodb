227e4e3e00c5ac9efd8f8c4d7cd686ad20671fa9
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2024-08-29 18:44:25 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/courseJava/workshopmongo/domain/User.java
@@ -0,0 +1,64 @@
+package com.courseJava.workshopmongo.domain;
+
+import java.util.Objects;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(id, user.id)) return false;
+        if (!Objects.equals(name, user.name)) return false;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
