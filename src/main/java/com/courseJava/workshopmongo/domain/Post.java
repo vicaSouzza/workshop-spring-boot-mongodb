1d9aa20d4e469c9e3e6b2be5706cc80bf36b3387
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2024-08-29 23:12:20 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/courseJava/workshopmongo/domain/Post.java
+++ src/main/java/com/courseJava/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.courseJava.workshopmongo.domain;
 
+import com.courseJava.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -14,13 +15,13 @@ public class Post {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post(){
 
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -60,11 +61,11 @@ public class Post {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
