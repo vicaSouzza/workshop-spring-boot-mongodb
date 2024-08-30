a9b1040b8738a12f1498fa6db8f31df4dd4b1319
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2024-08-29 21:59:17 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/courseJava/workshopmongo/resources/UserResource.java
+++ src/main/java/com/courseJava/workshopmongo/resources/UserResource.java
@@ -43,4 +43,10 @@ public class UserResource {
 
     }
 
+    @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
