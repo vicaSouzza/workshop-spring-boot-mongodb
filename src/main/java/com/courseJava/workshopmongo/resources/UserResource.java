a05e1d47e808a53545c7df9aab3fc752bcb1179c
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2024-08-29 21:50:42 -0300

    Inserção de usuário com POST

--- src/main/java/com/courseJava/workshopmongo/resources/UserResource.java
+++ src/main/java/com/courseJava/workshopmongo/resources/UserResource.java
@@ -6,11 +6,10 @@ import com.courseJava.workshopmongo.dto.UserDTO;
 import com.courseJava.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,6 +34,13 @@ public class UserResource {
         return ResponseEntity.ok().body(new UserDTO(obj));
 
     }
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
 
+    }
 
 }
--- src/main/java/com/courseJava/workshopmongo/services/UserService.java
+++ src/main/java/com/courseJava/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.courseJava.workshopmongo.services;
 
 
 import com.courseJava.workshopmongo.domain.User;
+import com.courseJava.workshopmongo.dto.UserDTO;
 import com.courseJava.workshopmongo.repository.UserRepository;
 import com.courseJava.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,4 +26,12 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
   }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
 }
